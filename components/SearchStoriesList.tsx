b9ecaa6460b52830bcc38c571007ef8c4149c8af
rjm13 <65667098+rjm13@users.noreply.github.com>
2022-01-14 00:19:19 -0600

    search function for stories

--- components/SearchStoriesList.tsx
+++ components/SearchStoriesList.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext, useRef} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { 
     View, 
     StyleSheet, 
@@ -8,21 +8,15 @@ import {
     RefreshControl, 
     TouchableWithoutFeedback, 
     TouchableOpacity, 
-    Image, 
-    TextInput,
-    Keyboard
+    Image 
 } from 'react-native';
 
 import {useNavigation} from '@react-navigation/native'
 
-import { Searchbar } from 'react-native-paper';
-
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import {LinearGradient} from 'expo-linear-gradient';
-
-import SearchStoriesList from '../components/SearchStoriesList';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 import { AppContext } from '../AppContext';
 
@@ -30,62 +24,10 @@ import { listPinnedStories, listRatings, listStories } from '../src/graphql/quer
 import { deletePinnedStory } from '../src/graphql/mutations';
 import {graphqlOperation, API, Auth} from 'aws-amplify';
 
-const SearchScreen = ({navigation} : any) => {
 
-  //search function states
-    const [newSearch, setNewSearch] = useState('');
+const SearchStoriesList = ({search} : any) => {
 
-    //search function trigger that refreshes the search results
-    const [didUpdate, setDidUpdate] = useState(false);
-
-    //focus the keyboard only on initial render
-    const focus = useRef(null)
-
-    useEffect(() => {
-      focus.current.focus()
-    }, [])
-
-  //this is the search bar
-    function SearchBar () {
-
-        const [searchQuery, setSearchQuery] = useState('');
-
-        const onChangeSearch = (query : any)  => setSearchQuery(query); 
-
-        return (
-          <View>
-            <Searchbar
-              placeholder={'Search Stories'}
-              placeholderTextColor='#000000a5'
-              autoComplete={true}
-              onChangeText={onChangeSearch}
-              onIconPress={() => {setNewSearch(searchQuery); setDidUpdate(!didUpdate); }}
-              onSubmitEditing={() => {setNewSearch(searchQuery); setDidUpdate(!didUpdate);}}
-              value={searchQuery}
-              ref={focus}
-              maxLength={20}
-              icon={() => {return(
-                <FontAwesome5 
-                  name='search'
-                  color='#000000a5'
-                  size={18}
-                />)}}
-              iconColor='#000000a5'
-              style={{
-                height: 35,
-                marginLeft: 40,
-                borderRadius: 8,
-                backgroundColor: '#e0e0e0',
-                width: Dimensions.get('window').width - 100 
-              }}
-              inputStyle={{fontSize: 16,}}
-            />
-          </View>
-        );
-      };
-
-      //this is the rendered search result item
-      const Item = ({title, genre, description, imageUri, nsfw, audioUri, author, narrator, time, id} : any) => {
+    const Item = ({title, genre, description, imageUri, nsfw, audioUri, author, narrator, time, id} : any) => {
         
         const Colors = {
             color: 
@@ -367,225 +309,175 @@ const SearchScreen = ({navigation} : any) => {
         );
     }
 
-    //primary data set of searched stories for the flatlist
+
+
+    //state for the array of pinned stories for that user
     const [searchedStories, setSearchedStories] = useState([])
+
+    //update trigger for fetching the pinned stories
+    const [didUpdate, setDidUpdate] = useState(false);
 
     //on render, get the user and then list the following connections for that user
     useEffect(() => {
 
-      const fetchStories = async () => {
+        const fetchStories = async () => {
 
-          if (newSearch.length > 2 ) {
+            //const SearchResults = []
 
-          try {
+            console.log(search)
 
-              const searchResults = await API.graphql(graphqlOperation(
-                  listStories, {
-                      filter: {
-                        or: [
-                          {title: {
-                              contains: newSearch
-                          }},
-                          {description: {
-                            contains: newSearch
-                            }}
-                        ]
-                      }
-              }))
+            try {
 
-              setSearchedStories(searchResults.data.listStories.items);
+                const searchResults = await API.graphql(graphqlOperation(
+                    listStories, {
+                        filter: {
+                            title: {
+                                contains: search
+                            }
+                        }
+                }))
 
-          } catch (e) {
-          console.log(e);
+                setSearchedStories(searchResults.data.listStories.items);
+
+            } catch (e) {
+            console.log(e);
+          }
         }
-      } else {
-        return;
+           fetchStories(); 
+      }, [didUpdate])
+
+
+    const [isFetching, setIsFetching] = useState(false);
+
+    const onRefresh = () => {
+        setIsFetching(true);
+        setDidUpdate(!didUpdate)
+        setTimeout(() => {
+          setIsFetching(false);
+        }, 2000);
       }
-    }
-         fetchStories(); 
-    
-    }, [didUpdate])
 
     const renderItem = ({ item } : any) => (
 
-      <Item 
-        title={item.title}
-        imageUri={item.imageUri}
-        genre={item.genre}
-        audioUri={item.audioUri}
-        description={item.description}
-        author={item.author}
-        narrator={item.narrator}
-        time={item.time}
-        id={item.id}
-        nsfw={item.nsfw}
-        //liked={item.liked}
-        //rating={item.rating}
-      />
-    );
-
+        <Item 
+          title={item.title}
+          imageUri={item.imageUri}
+          genre={item.genre}
+          audioUri={item.audioUri}
+          description={item.description}
+          author={item.author}
+          narrator={item.narrator}
+          time={item.time}
+          id={item.id}
+          nsfw={item.nsfw}
+          //liked={item.liked}
+          //rating={item.rating}
+        />
+      );
 
     return (
-        <View >
-          <LinearGradient
-          colors={['#363636','#2f217966', '#000']}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-        >
-          <View style={{marginTop: 40, marginBottom: 10, marginHorizontal: 20}}>
-            <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-                <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
-                  <View style={{padding: 30, margin: -30}}>
-                    <FontAwesome5 
-                        name='chevron-left'
-                        color='#fff'
-                        size={20}
-                    />
-                  </View>
-                </TouchableWithoutFeedback>
-                
-                <SearchBar />
-                
-            </View>
-            <View style={{ flexDirection: 'row'}}>
-            
-            </View>
-          </View>
+            <View style={styles.container}>
 
-            
+                <FlatList 
+                    data={searchedStories}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    extraData={searchedStories}
+                    refreshControl={
+                        <RefreshControl
+                        refreshing={isFetching}
+                        onRefresh={onRefresh}
+                        />
+                    }
+                    showsVerticalScrollIndicator={false}    
+                    ListFooterComponent={ () => {
+                        return (
+                            <View style={{ height:  70, alignItems: 'center'}}>
+                                <Text style={{ color: 'white', margin: 20,}}>
+                                    
+                                </Text>
+                            </View>
+                    );}}
+                    ListHeaderComponent={ () => {
+                        return (
+                            <View style={{ height:  20, alignItems: 'center'}}>
+                                <Text style={{ color: 'white', margin: 20,}}>
+                                    
+                                </Text>
+                            </View>
+                    );}}
+                    ListEmptyComponent={ () => {
+                        return (
+                            <View style={{ height:  70, alignItems: 'center'}}>
+                                <Text style={{ color: 'white', margin: 20,}}>
+                                </Text>
+                            </View>
+                    );}}
+                />
 
-            <View style={{ alignSelf: 'center',marginHorizontal: 0, height: '90%'}}>
-              <FlatList 
-                data={searchedStories}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-                extraData={searchedStories}
-                // refreshControl={
-                //     <RefreshControl
-                //     refreshing={isFetching}
-                //     onRefresh={onRefresh}
-                //     />
-                // }
-                showsVerticalScrollIndicator={false}   
-                initialNumToRender={12}
-                maxToRenderPerBatch={10} 
-                ListFooterComponent={ () => {
-                    return (
-                        <View style={{ height:  70, alignItems: 'center'}}>
-                            <Text style={{ color: 'white', margin: 20,}}>
-                                
-                            </Text>
-                        </View>
-                );}}
-                ListHeaderComponent={ () => {
-                    return (
-                        <View style={{ height:  20, alignItems: 'center'}}>
-                            <Text style={{ color: 'white', margin: 20,}}>
-                                
-                            </Text>
-                        </View>
-                );}}
-                ListEmptyComponent={ () => {
-                    return (
-                        <View style={{ height:  70, alignItems: 'center'}}>
-                            <Text style={{ color: 'white', margin: 20,}}>
-                            </Text>
-                        </View>
-                );}}
-            />
             </View>
 
-        </LinearGradient>
-        </View>
     );
 }
 
-const styles = StyleSheet.create ({
-  container: {
-    width: Dimensions.get('window').width, 
- },
-  header: {
-      color: '#fff',
-      fontSize: 22,
-      fontWeight: 'bold',
-  },
-  title: {
-    color: '#000',
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  box: {
-    height: 100,
-    width: 140,
-    borderRadius: 15,
-    marginVertical: 10,
-    padding: 10,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  genrebox: {
-    height: 80,
-    borderRadius: 15,
-    marginVertical: 10,
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  tile: {
-    backgroundColor: '#363636a5',
-    marginHorizontal: 10,
-    marginVertical: 5,
-    padding: 20,
-    borderRadius: 15,
-},
-name: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#fff',
-    flexWrap: 'wrap',
-    width: 225,
-},
-userId: {
-    fontSize: 12,
-    color: '#ffffffa5',
-    marginRight: 15,
-    marginLeft: 5,
-},
-icontext: {
-    fontSize: 10,
-    color: '#ffffffa5',
-    marginTop: 5,
-},
-popupblock: {
-    marginTop: 10,
-},
-paragraph: {
-    color: '#ffffffB3'
-},
-playbutton: {
-    borderWidth: 0.5,
-    paddingHorizontal: 15,
-    paddingVertical: 3,
-    borderRadius: 15,
-    borderColor: '#ffffffa5',
-    color: '#ffffffa5',
-},
-time: {
-    fontSize: 14,
-    fontWeight: 'normal',
-    color: '#ffffffa5',
-    marginHorizontal: 5,
-},
-category: {
-    fontSize: 14,
-    color: 'cyan',
-    //fontStyle: 'italic',
-    marginVertical: 3,
-    textTransform: 'capitalize'
+const styles = StyleSheet.create({
+    container: {
+       width: Dimensions.get('window').width, 
+    },
+    tile: {
+        backgroundColor: '#363636a5',
+        marginHorizontal: 10,
+        marginVertical: 5,
+        padding: 20,
+        borderRadius: 15,
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#fff',
+        flexWrap: 'wrap',
+        width: 225,
+    },
+    userId: {
+        fontSize: 12,
+        color: '#ffffffa5',
+        marginRight: 15,
+        marginLeft: 5,
+    },
+    icontext: {
+        fontSize: 10,
+        color: '#ffffffa5',
+        marginTop: 5,
+    },
+    popupblock: {
+        marginTop: 10,
+    },
+    paragraph: {
+        color: '#ffffffB3'
+    },
+    playbutton: {
+        borderWidth: 0.5,
+        paddingHorizontal: 15,
+        paddingVertical: 3,
+        borderRadius: 15,
+        borderColor: '#ffffffa5',
+        color: '#ffffffa5',
+    },
+    time: {
+        fontSize: 14,
+        fontWeight: 'normal',
+        color: '#ffffffa5',
+        marginHorizontal: 5,
+    },
+    category: {
+        fontSize: 14,
+        color: 'cyan',
+        //fontStyle: 'italic',
+        marginVertical: 3,
+        textTransform: 'capitalize'
 
-},
+    },
+
 });
 
-export default SearchScreen;
+export default SearchStoriesList;
