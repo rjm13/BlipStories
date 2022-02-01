5dd362b165c939aa4e93264095df497bd6eb8da9
rjm13 <65667098+rjm13@users.noreply.github.com>
2022-02-01 14:24:33 -0600

    restrict file types

--- screens/EditAudioStory.tsx
+++ screens/EditAudioStory.tsx
@@ -247,7 +247,7 @@ const [localImageUri, setLocalImageUri] = useState('');
 //image picker
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
         aspect: [4, 3],
         quality: 1,
--- screens/EditProfileScreen.tsx
+++ screens/EditProfileScreen.tsx
@@ -48,7 +48,7 @@ const EditProfile = ({navigation} : any) => {
 //pick the image from the camera roll
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
